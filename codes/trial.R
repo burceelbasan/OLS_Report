55065084fee236fa11d8ed56719d164b00d95849
burceelbasan <90277168+burceelbasan@users.noreply.github.com>
2022-01-24 00:19:35 +0100

    Create trial.R

---
+++ codes/trial.R
@@ -0,0 +1,30 @@
+# Reading excel file
+Data3_post_mentee_Renamed_1q <- read_xlsx("Data3_post_mentee_Renamed_1q.xlsx")
+  view(Data3_post_mentee_Renamed_1q)
+# Converting Responses to Percentages  
+  Data3_post_mentee_Renamed_1q <-   Data3_post_mentee_Renamed_1q %>%   
+    mutate(percent_answers = Responses / 35) 
+  View(Data3_post_mentee_Renamed_1q)
+  
+  Data3_post_mentee_Renamed_1q  <-  Data3_post_mentee_Renamed_1q %>%   
+    mutate(numbers = c("Q1","Q2","Q3"))
+  View(  Data3_post_mentee_Renamed_1q)
+  
+  p <-   Data3_post_mentee_Renamed_1q %>% 
+    ggplot(aes(x = numbers, y= percent_answers, fill = numbers, label = scales::percent(percent_answers), labels = numbers))+ 
+    geom_bar(stat="identity", color="black")+ 
+    geom_text(position = position_dodge(width = .9),    # move to center of bars
+              vjust = -0.5,    # nudge above top of bar
+              size = 3)+ 
+    scale_fill_manual(values = wes_palette(3, name = "Royal1",type = "continuous"), name = "") +
+    xlab("Questions")+
+    ylab("Percentage of Answers")+
+    ggtitle("How was your overall project leadership experience in OLS-3?")+
+    scale_y_continuous(labels = scales::percent)
+  
+  p
+  
+  p+ scale_fill_discrete(name="Questions",
+                         breaks=c("Q1","Q2","Q3"),
+                         labels=c("I was able to meet ALL my goals","I was able to meet MOST of my goals","I was able work on my project but only PARTIALLY"))
+  
